164bda136614fa8f71b0db98aaf7c16bd2c7142e
adrian.schneider@aot.swiss <adrian.schneider@aot.swiss>
2018-12-12 17:18:59 +0100

    Use lists

--- basics/controls.py
+++ basics/controls.py
@@ -22,3 +22,24 @@ print( "a = " + str(a) + ",  b = " + str(b))
 for pos in range(20):
     formatStr = "%." + str(pos) + "f"
     print(formatStr % math.pi)
+
+myFloat = int(1.0)
+if isinstance( myFloat, int):
+    print( "myFloat is int")
+else:
+    if isinstance( myFloat, float ):
+        print( "myFloat is float")
+    else:
+        print( "myFloat is unknown")
+
+
+# list
+
+myMixedList = []
+myMixedList.append(1)
+myMixedList.append(2)
+myMixedList.append(3.1)
+myMixedList.append("abc")
+
+for ele in myMixedList:
+    print(ele)
