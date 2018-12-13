cac23ccb1fc712b291d385d5fe1ffaee5cf999f4
adrian.schneider@aot.swiss <adrian.schneider@aot.swiss>
2018-12-13 09:29:38 +0100

    Using numpy

---
+++ numpyarr/np.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+height = [1.82,  1.87, 1.82, 1.91, 1.90, 1.85]
+weight = [81, 97.52, 95.25, 92.98, 86.18, 88.45]
+
+np_height = np.array(height)
+np_weight = np.array(weight)
+
+bmi = np_weight / (np_height ** 2)
+
+# Print the result
+print(bmi)
+
+print(bmi > 23)
+
+print(bmi[bmi > 26])
+
+# matrix mul
+a = np.array([[1.0, 2.0], [3.0, 4.0]])
+v = np.array([[1.0], [4.0]])
+print(a)
+print(v)
+
+print(np.matmul(a,v))
+
+
+
+
+
