ab6b03e6e12dbe05298444ae1e831983bfd3485f
Adrian Schneider <adrian.schneider.pr@gmail.com>
2025-08-14 22:04:47 +0200

    start two egg

---
+++ problems/2-egg/main.py
@@ -0,0 +1,26 @@
+import random
+
+
+class EggGame:
+
+    def __init__(self, n_eggs: int, n_stories: int):
+        self.n_eggs_remaining = n_eggs
+
+        random.randint(0, 100)
+
+
+
+
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
